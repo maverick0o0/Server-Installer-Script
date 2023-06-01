33b604e01f0567f92276efa287ad5a786ce159ec
Maverick <tavakoly123@gmail.com>
2023-06-01 14:34:16 +0330

    Adding gospider

--- setup-installer.sh
+++ setup-installer.sh
@@ -47,6 +47,7 @@ go install github.com/tomnomnom/anew@latest &> /dev/null || echo "Failed to inst
 go install github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest &> /dev/null || echo "Failed to install nuclei."
 go install github.com/tomnomnom/waybackurls@latest&> /dev/null || echo "Failed to install waybackurls."
 go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest || echo "Failed to install waybackurls."
+go install github.com/jaeles-project/gospider@latest || echo "Failed to install gospider."
 wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz;rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz;export PATH=$PATH:/usr/local/go/bin;source $HOME/.profile;go version;
 
 
