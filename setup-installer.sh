3225050c067f58dfbc26ecdf66b9c3729c3b3cdd
Maverick <tavakoly123@gmail.com>
2023-06-04 16:02:47 +0330

    fix problem

--- setup-installer.sh
+++ setup-installer.sh
@@ -6,8 +6,8 @@ error_exit() {
 
 echo "Installing packages..."
 # Install packages
-sudo apt update || error_exit "Failed to update package repositories."
-sudo apt install -y tmux crunch apache2 php php-curl certbot python3-certbot-apache git net-tools libpcap-dev build-essential libssl-dev curl jq zip || error_exit "Failed to instal>
+sudo apt update && apt upgrade || error_exit "Failed to update package repositories."
+sudo apt install -y tmux crunch apache2 php php-curl certbot python3-certbot-apache git net-tools libpcap-dev build-essential libssl-dev curl jq zip || error_exit "Failed to install deps"
 
 echo "Installing Python..."
 # Install Python
